aa6414b82eb6d679df89179e2d1345876172cca5
July Pilowsky <pilowskyj@derecho7.hsn.de.hpc.ucar.edu>
2024-10-10 11:09:11 -0600

    Debug script now runs correctly

--- Scripts/validation_debug.R
+++ Scripts/validation_debug.R
@@ -4,13 +4,13 @@ library(here)
 library(readr)
 library(doParallel)
 library(foreach)
-i_am("mgsim/Scripts/round1_validation.qmd")
+i_am("mgsim/Scripts/validation_debug.R")
 source("/glade/u/home/pilowskyj/mgsim/Scripts/validation_metric_functions.R")
 # Register parallel backend with doParallel
 num_cores <- 120
-cl <- makeCluster(num_cores)
+cl <- makeCluster(num_cores, type="FORK", outfile="")
 registerDoParallel(cl)
-results_dir <- system("cd /glade/work/pilowskyj/Round1_matrix; ls -f", intern = T) |>
+results_dir <- system("cd /glade/derecho/scratch/pilowskyj/Round1; ls -f", intern = T) |>
   gtools::mixedsort() |> _[-c(1:2)]
 round1_priors <- read_csv(here("mgsim/Data_minimal/Input/sample_data_round1.csv"))
 year_lookup <- data.frame(index = 1:77, Year = 1940:2016)
@@ -21,17 +21,29 @@ presabs <- here("mgsim/Data_minimal/Validation/haemorhous_presence_absence.csv")
 
 # Gather relevant data
 presence_list <- results_dir[dc] |>
-  map(\(f) paste0("/glade/work/pilowskyj/Round1_matrix/", f)) |>
+  map(\(f) paste0("/glade/derecho/scratch/pilowskyj/Round1/", f)) |>
   lapply(list.files, full.names = TRUE) |>
   lapply(function(p) {
     data.frame(
       path = p,
-      index = as.numeric(str_split_i(p, "/", 7) |> str_split_i("_", 1)),
+      index = as.numeric(str_split_i(p, "/", 8) |> str_split_i("_", 1)),
       infected = str_detect(p, "I")
     ) |>
       arrange(index) |>
       left_join(year_lookup, by = join_by(index))
   })
+
+# Check presence_list
+print(paste("Length of presence_list:", length(presence_list)))
+flush.console()
+
+# Check the first element of presence_list (if not too large)
+if (length(presence_list) > 0) {
+  print("First element of presence_list:")
+  print(head(presence_list[[1]]))
+  flush.console()
+}
+
 presabs_metric <- foreach(
   plist = presence_list,
   .combine = c,
@@ -43,23 +55,15 @@ presabs_metric <- foreach(
     flush.console()
 
     # Original code
+
     plist_filled <- plist |> fill_missing_years(1940, 2016) |> group_split(Year)
     sim_array <- array(0, dim = c(106, 161, length(plist_filled)))
 
     # Adding together the matrices for all age classes and disease compartments for each year
     for (i in seq_along(plist_filled)) {
       df <- plist_filled[[i]]
-
-      # Print the year and number of paths being processed
-      print(paste("Processing year:", df$Year[1], "with", length(df$path), "files at", Sys.time()))
-      flush.console()
-
       combined_files <- Reduce(`+`, lapply(df$path, read_or_zero))
       sim_array[, , i] <- combined_files
-
-      # Print after processing each year
-      print(paste("Processed year", i, "at:", Sys.time()))
-      flush.console()
     }
 
     # Calculating presence/absence penalty
@@ -92,3 +96,6 @@ presabs_metric <- foreach(
     return(NA)
   })
 }
+
+presabs <- data.frame(sim = c(1:10000)[dc], hm_presabs = presabs_metric)
+write_csv(presabs, "/glade/u/home/pilowskyj/mgsim/Data_minimal/Validation/presabs.csv")
